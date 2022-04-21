9a4de06eb351ae1039413b23f67fbb9bf438daa7
Valentin <valentinbaudry64@gmail.com>
2022-04-21 14:45:51 +0200

    unit test

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -8,4 +8,28 @@ class TempTrackerTest extends TestCase {
     public function test_something() {
         $this->assertEquals(42, 42);
     }
+
+    public function test_insert_negative_temp() {
+        $this->expectException(TypeError::class);
+        $this->expectExceptionMessage('parameter should be a positive int');
+
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(-1);
+    }
+
+    public function test_insert_temp_out_of_range() {
+        $this->expectException(ValueError::class);
+        $this->expectExceptionMessage('parameter should be within [0..110]');
+
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert(111);
+    }
+
+    public function test_insert_temp_not_int() {
+        $this->expectException(TypeError::class);
+        $this->expectExceptionMessage('parameter should be an int');
+
+        $temp_tracker = new TempTracker();
+        $temp_tracker->insert('1');
+    }
 }
--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -10,4 +10,29 @@ class FlattenTest extends TestCase
     {
         $this->assertEquals(1337, 1337);
     }
+
+    public function test_flatten_empty_array()
+    {
+        $this->assertEquals(flatten([]), []);
+    }
+
+    public function test_flatten_array_with_one_element()
+    {
+        $this->assertEquals(flatten([1]), [1]);
+    }
+
+    public function test_flatten_array_with_two_elements()
+    {
+        $this->assertEquals(flatten([1, 2]), [1, 2]);
+    }
+
+    public function test_flatten_array_two_dimensions()
+    {
+        $this->assertEquals(flatten([1, 2, [3, 4]]), [1, 2, 3, 4]);
+    }
+
+    public function test_flatten_array_three_dimensions()
+    {
+        $this->assertEquals(flatten([1, 2, [3, 4, [5, 6]]]), [1, 2, 3, 4, 5, 6]);
+    }
 }
